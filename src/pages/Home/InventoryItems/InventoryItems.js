817ea4cdeaf437f160280d51178d1624e98603ff
Samritivij <101784176+Samritivij@users.noreply.github.com>
2024-09-15 12:35:34 +0530

    Create InventoryItems.js

---
+++ src/pages/Home/InventoryItems/InventoryItems.js
@@ -0,0 +1,32 @@
+import { Button, Container, Row } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import useInventoryItems from '../../../hooks/useInventoryItems';
+import SingleItem from './SingleItem';
+import image1 from '../../../images/cherry.png';
+import image2 from '../../../images/fruits.png';
+import Loading from '../../Shared/Loading/Loading';
+
+
+
+const InventoryItems = () => {
+    const [items] = useInventoryItems();
+    return (
+        <div>
+            <Container className='my-5'>
+                <h1 className='text-center fw-bold display-4'>Warehouse items <img src={image1} alt="" /></h1>
+                {
+                    items.length ?
+                        <Row>
+                            {
+                                items.slice(0, 6).map(item => <SingleItem key={item._id} item={item}></SingleItem>)
+
+                            }
+
+                        </Row> : <Loading></Loading>}
+                <Link to='/manageinventories'><Button className='btn-lg mx-auto d-block text-white my-5 hover1' variant='danger'>Manage Inventories <img className='ms-1' src={image2} alt="" /></Button></Link>
+            </Container>
+        </div>
+    );
+};
+
+export default InventoryItems;
